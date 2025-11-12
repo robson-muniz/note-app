d4a8da3d37be8549a7b6f0cf0dcf9e659d1ff32e
robson-muniz <robson_muniz@yahoo.com>
2025-11-12 09:44:38 +0000

    A couple of improves in the controled imput syntase

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     return (
         <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold mb-4 text-center">
-            📝 Notes App
+            📝 Notes App by Robson
         </h2>
             <NoteForm notes={notes} setNotes={setNotes} />
         </div>
