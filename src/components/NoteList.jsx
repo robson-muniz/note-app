1ba68aaac3c60ef6c65402d07db34ccbf0f0c352
robson-muniz <robson_muniz@yahoo.com>
2025-11-17 15:37:59 +0000

    Show Notes

---
+++ src/components/NoteList.jsx
@@ -0,0 +1,34 @@
+const NoteList = ({notes}) => {
+    if (notes.length === 0) {
+        return (
+            <p className="text-center text-gray-500">
+                No notes found.
+            </p>
+        )
+    }
+
+    return (
+        <div className="space-y-4">
+            {notes.map((note, i) => (
+                <div className="p-4 bg-white rounded-l shadow-md border-l-4"
+                key={notes.id}>
+                    <h3 className="text-lg font-bold">
+                        {note.title}
+                    </h3>
+                    <p className="text-sm text-gray-600">
+                        <strong>Category:</strong> {note.category}
+                    </p>
+                    <p className="text-sm text-gray-600">
+                        <strong>Priority:</strong> {note.priority}
+                    </p>
+                    <p className="mt-2">
+                        {note.description}
+                    </p>
+
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default NoteList;
